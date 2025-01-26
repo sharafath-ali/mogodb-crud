b0c050b15dba35501eba40cfabb594cfaad7a7f7
sharafath ali <sharafathaliofficial@gmail.com>
2025-01-26 23:46:14 +0530

    bug fixed

--- db.js
+++ db.js
@@ -1,50 +1,50 @@
-// import { MongoClient } from "mongodb";
-
-// const uri = "mongodb://localhost:27017/";
-
-// let dbconnection;
-// const connectDb = async (cb) => {
-//    MongoClient.connect(uri).then((client) => {
-//       dbconnection = client.db();
-//       return cb();
-//    }).catch((err) => {
-//       console.log(err);
-//       return cb(err);
-//    });
-// }
-
-// const getDb = () => {
-//    if (dbconnection) {
-//       return dbconnection;
-//    }
-// }
-
-// export {
-//    connectDb,
-//    getDb
-// }
 import { MongoClient } from "mongodb";
 
 const uri = "mongodb://localhost:27017/";
-let dbconnection;
 
+let dbconnection;
 const connectDb = async (cb) => {
-  try {
-    const client = await MongoClient.connect(uri, { useUnifiedTopology: true });
-    dbconnection = client.db("school"); // Replace with your database name
-    console.log("Database connected successfully");
-    cb();
-  } catch (err) {
-    console.error("Database connection failed", err);
-    cb(err);
-  }
-};
+   MongoClient.connect(uri).then((client) => {
+      dbconnection = client.db();
+      return cb();
+   }).catch((err) => {
+      console.log(err);
+      return cb(err);
+   });
+}
 
 const getDb = () => {
-  if (dbconnection) {
-    return dbconnection;
-  }
-  throw new Error("Database not initialized. Call connectDb first.");
-};
+   if (dbconnection) {
+      return dbconnection;
+   }
+}
 
-export { connectDb, getDb };
+export {
+   connectDb,
+   getDb
+}
+// import { MongoClient } from "mongodb";
+
+// const uri = "mongodb://localhost:27017/";
+// let dbconnection;
+
+// const connectDb = async (cb) => {
+//   try {
+//     const client = await MongoClient.connect(uri, { useUnifiedTopology: true });
+//     dbconnection = client.db("school"); // Replace with your database name
+//     console.log("Database connected successfully");
+//     cb();
+//   } catch (err) {
+//     console.error("Database connection failed", err);
+//     cb(err);
+//   }
+// };
+
+// const getDb = () => {
+//   if (dbconnection) {
+//     return dbconnection;
+//   }
+//   throw new Error("Database not initialized. Call connectDb first.");
+// };
+
+// export { connectDb, getDb };
